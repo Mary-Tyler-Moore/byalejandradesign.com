0a828157685d25ea4fea3b8bc6e233111444033c
njmyers <17010801+njmyers@users.noreply.github.com>
2018-05-21 22:07:27 -0400

    debug

--- web/app/themes/artetexture/functions.php
+++ web/app/themes/artetexture/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-include(get_template_directory() . '/acf-json.php');
-include(get_template_directory() . '/image-sizes.php');
+include get_template_directory() . '/acf-json.php';
+include get_template_directory() . '/image-sizes.php';
 
 ?>
