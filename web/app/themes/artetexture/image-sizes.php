6be2728d5b928891f04a35f7f0bdd47b923e11ed
njmyers <17010801+njmyers@users.noreply.github.com>
2018-05-21 22:48:01 -0400

    added custom image sizes

--- web/app/themes/artetexture/image-sizes.php
+++ web/app/themes/artetexture/image-sizes.php
@@ -1,3 +1,15 @@
 <?php
 
+function add_medium_square() {
+    add_image_size( 'medium-square', 600, 600, true );
+}
+
+add_action( 'after_setup_theme', 'add_medium_square' );
+
+function add_small_square() {
+    add_image_size( 'small-square', 350, 350, true );
+}
+
+add_action( 'after_setup_theme', 'add_small_square' );
+
 ?>
