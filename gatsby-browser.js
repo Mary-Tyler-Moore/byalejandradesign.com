c634539f36b2dbc1032e73c7406b1d6680d8afdd
njmyers <17010801+njmyers@users.noreply.github.com>
2018-06-26 11:03:08 -0400

    turned off redux code for now

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import { Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import createStore from './src/store';
-
-export const replaceRouterComponent = ({ history }) => {
-  const store = createStore();
-
-  const ConnectedRouterWrapper = ({ children }) => (
-    <Provider store={store}>
-      <Router history={history}>{children}</Router>
-    </Provider>
-  );
-
-  return ConnectedRouterWrapper;
-};
+// import * as React from 'react';
+// import { Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+//
+// import createStore from './src/store';
+//
+// export const replaceRouterComponent = ({ history }) => {
+//   const store = createStore();
+//
+//   const ConnectedRouterWrapper = ({ children }) => (
+//     <Provider store={store}>
+//       <Router history={history}>{children}</Router>
+//     </Provider>
+//   );
+//
+//   return ConnectedRouterWrapper;
+// };
