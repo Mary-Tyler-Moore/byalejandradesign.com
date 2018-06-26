00f32c0c6d23ff1ff3999253c9b49c74e87de437
njmyers <17010801+njmyers@users.noreply.github.com>
2018-06-26 10:55:57 -0400

    babelrc is breaking gatsby

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,10 +1,9 @@
-import thunk from 'redux-thunk';
 import { createEpicMiddleware } from 'redux-observable';
 import { applyMiddleware, compose } from 'redux';
 import rootEpic from './root-epic';
 
 const epicMiddleware = createEpicMiddleware(rootEpic);
-const middlewares = [thunk, epicMiddleware];
+const middlewares = [epicMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   const { logger } = require('redux-logger');
