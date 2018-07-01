4e04787f41a23ce3db8fa331a2835ebb7c4cf78f
njmyers <17010801+njmyers@users.noreply.github.com>
2018-07-01 18:53:56 -0400

    cleaned up braintree actions jsdocs

--- src/components/Checkout/Braintree/braintree-actions.js
+++ src/components/Checkout/Braintree/braintree-actions.js
@@ -2,6 +2,7 @@
 /**
  * Saves an authorized braintree client to redux state
  * @param {object} payload an authorized created braintree client
+ * @return {action}         action object
  */
 export const saveClient = (payload: {}) => ({
   type: '@BRAINTREE/SAVE_CLIENT',
@@ -11,6 +12,7 @@ export const saveClient = (payload: {}) => ({
 /**
  * Saves a data collector instance to redux state
  * @param {object} payload dataCollector from braintree
+ * @return {action}         action object
  * https://developers.braintreepayments.com/guides/advanced-fraud-tools/client-side/javascript/v3
  */
 export const saveCollector = (payload: {}) => ({
@@ -18,21 +20,28 @@ export const saveCollector = (payload: {}) => ({
   payload,
 });
 
+/**
+ * Creates an action to save device data to redux state
+ * @param  {object} payload device data from the BT collector
+ * @return {action}         action object
+ */
 export const deviceData = (payload: {}) => ({
   type: '@BRAINTREE/DEVICE_DATA',
   payload,
 });
 
+/**
+ * Print errors to console directly during development
+ * @param  {error} err  error of unknown type
+ */
 const logErrors = (err) => {
   if (process.env.NODE_ENV !== 'production') console.error(err);
 };
 
 /**
- * Log errors to redux
- * @param  {[type]} message [description]
- * @param  {[type]} err     [description]
- * @param  {[type]} status  [description]
- * @return {[type]}         [description]
+ * Logs errors of unknown type to redux state
+ * @param  {error} error  error of unknown type
+ * @return {action}       action object
  */
 export const braintreeError = (error: Error) => {
   logErrors(error);
