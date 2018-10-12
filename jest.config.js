ff872cde319a5c0575988343c87e3b15e69fa0d6
njmyers <17010801+njmyers@users.noreply.github.com>
2018-10-12 11:14:49 -0400

    updated config to run jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/src/__mocks__/fileMock.js',
-    '\\.(sass|css|scss)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/src/__mocks__/file-mock.js',
+    '\\.(sass|css|scss)$': '<rootDir>/src/__mocks__/style-mock.js',
   },
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.cache/'],
-  transform: { '^.+\\.js$': '<rootDir>/jest-preprocess.js' },
+  testPathIgnorePatterns: [
+    'node_modules/',
+    '<rootDir>/.cache/',
+    '<rootDir>/public',
+    'helpers/',
+  ],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
 };
