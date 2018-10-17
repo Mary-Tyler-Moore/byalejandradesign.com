21c63b98e2a7d3ea3595a1ca79b1b9c72376ee58
njmyers <17010801+njmyers@users.noreply.github.com>
2018-10-16 21:43:53 -0400

    adding stages to checkout-server

--- packages/checkout-server/src/routes/braintree/gateway.js
+++ packages/checkout-server/src/routes/braintree/gateway.js
@@ -1,9 +1,12 @@
 import braintree from 'braintree';
 
-console.log(process.env.BRAINTREE_MERCHANT_ID);
+const stage = process.env.STAGE;
 
 const gateway = braintree.connect({
-  environment: braintree.Environment.Sandbox,
+  environment:
+    stage !== 'production '
+      ? braintree.Environment.Sandbox
+      : braintree.Environment.Production,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
