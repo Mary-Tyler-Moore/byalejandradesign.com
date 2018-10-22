14f71243f5697099c11b87d301cbbced41aaaad9
njmyers <17010801+njmyers@users.noreply.github.com>
2018-10-22 16:19:25 -0400

    added api key to requests

--- packages/artetexture.com/src/components/Checkout/server.js
+++ packages/artetexture.com/src/components/Checkout/server.js
@@ -8,8 +8,21 @@ const base = () => {
   return process.env.GATSBY_BRAINTREE_SERVER;
 };
 
+const key = () => {
+  if (!process.env.GATSBY_API_KEY) {
+    console.warn('You must provide a GATSBY_API_KEY in .env');
+  }
+
+  return process.env.GATSBY_API_KEY;
+};
+
+console.log(process.env);
+
 const server = axios.create({
   baseURL: base(),
+  headers: {
+    Authorization: key(),
+  },
 });
 
 export default server;
