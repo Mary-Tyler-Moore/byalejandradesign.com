c6cabceaef9ecb1a38a063d0f70fb9a96053b5fb
njmyers <17010801+njmyers@users.noreply.github.com>
2018-10-23 15:07:51 -0400

    added order total to template

--- packages/templates/src/OrderConfirmation/OrderConfirmation.js
+++ packages/templates/src/OrderConfirmation/OrderConfirmation.js
@@ -1,5 +1,6 @@
 /** @flow */
 import * as React from 'react';
+import { dollarString } from 'smalldash';
 import Line from '../Line';
 import ServerAddress from '../ServerAddress';
 import Customer from '../Customer';
@@ -16,18 +17,28 @@ export type Props = {
 
 class OrderConfirmation extends React.Component<Props> {
   static defaultProps = {
-    transaction: transaction,
+    transaction,
   };
 
   render() {
     return (
       <section className="confirmation">
         <h3 className="h3-amiri">Order Confirmation</h3>
-        {this.props.transaction.orderId && (
-          <section>
+        <p className="body-sourceSans-2">
+          <em>
+            Thank you for your order from artetexture.com! Your order details
+            are summarized below.
+          </em>
+        </p>
+        <section>
+          <h4 className="h4-amiri">Order Info</h4>
+          {this.props.transaction.orderId && (
             <Line label="orderNumber">{this.props.transaction.orderId}</Line>
-          </section>
-        )}
+          )}
+          <Line label="total">
+            {dollarString(this.props.transaction.amount)}
+          </Line>
+        </section>
         <Customer customer={this.props.transaction.customer} />
         <ServerAddress
           address={this.props.transaction.shipping}
