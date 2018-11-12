4d2d3f1a981704ad63fd33be426560db38af1c8d
njmyers <17010801+njmyers@users.noreply.github.com>
2018-11-11 21:07:53 -0500

    final version of local plugin

--- packages/gatsby-source-wordpress/src/download-media-files.js
+++ packages/gatsby-source-wordpress/src/download-media-files.js
@@ -1,9 +1,50 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
-const { pipeAsync } = require('smalldash');
 
-const reuseFileNode = ({ e, fileNodeID, ...ctx }) =>
+/** Utils */
+
+/**
+ * Make sure to use async as function and not arrow function to prevent babel/rollup issues
+ * https://github.com/rollup/rollup/issues/1518
+ */
+const pipeAsync = (fn, ...fns) =>
+  async function(...args) {
+    return !fns.length
+      ? await fn(...args)
+      : pipeAsync(...fns)(await fn(...args));
+  };
+
+/**
+ * Ensures a promise resolution from the passed in function
+ * Retries after a timeout
+ */
+const downloadRunner = (fn) => (ctx, tries = 0) => {
+  const throttle = 2000;
+  const maxRetries = 10;
+
+  console.log(tries);
+  return new Promise((res, rej) => {
+    fn(ctx)
+      .then((ctx) => res(ctx))
+      .catch(() => {
+        if (tries < maxRetries) {
+          // if server timeout or the node doesn't resolve
+          // set a throttle and try again
+          setTimeout(() => res(downloadRunner(fn)(ctx, tries + 1)), throttle);
+        } else {
+          rej();
+        }
+      });
+  });
+};
+
+/** Original downloadMediaFiles logic broken into parts */
+
+/**
+ * Checks to ensure a file node is not already created
+ */
+const reuseFileNode = ({ e, ...ctx }) =>
   new Promise((res, rej) => {
-    // extract functions
+    // extract gatsby functions
     const { cache, touchNode } = ctx;
 
     const mediaDataCacheKey = `wordpress-media-${e.wordpress_id}`;
@@ -12,13 +53,20 @@ const reuseFileNode = ({ e, fileNodeID, ...ctx }) =>
       // If we have cached media data and it wasn't modified, reuse
       // previously created file node to not try to redownload
       if (cacheMediaData && e.modified === cacheMediaData.modified) {
-        fileNodeID = cacheMediaData.fileNodeID;
+        const fileNodeID = cacheMediaData.fileNodeID;
         touchNode({ nodeId: cacheMediaData.fileNodeID });
+        // fileNodeID is our flag in order to download a file
+        // add it to the ctx of the entity
+        res({ e, fileNodeID, mediaDataCacheKey, ...ctx });
+      } else {
+        res({ e, mediaDataCacheKey, ...ctx });
       }
-      res({ e, fileNodeID, mediaDataCacheKey, ...ctx });
     });
   });
 
+/**
+ * Requests a remote file node if fileNodeID is not present
+ */
 const downloadFile = ({ e, fileNodeID, mediaDataCacheKey, ...ctx }) =>
   new Promise((res, rej) => {
     // extract functions
@@ -46,31 +94,20 @@ const downloadFile = ({ e, fileNodeID, mediaDataCacheKey, ...ctx }) =>
               res({ e, fileNodeID, mediaDataCacheKey, ...ctx });
             });
         } else {
-          rej({ e, fileNodeID, mediaDataCacheKey, ...ctx });
+          // reject the promise here as the download has failed
+          rej();
         }
       });
     } else {
+      // resolve here as this means we already have a cached file
       res({ e, fileNodeID, mediaDataCacheKey, ...ctx });
     }
   });
 
-const downloadRunner = (ctx, tries = 0) => {
-  const maxRetries = 10;
-  console.log(tries);
-
-  return new Promise((res, rej) => {
-    downloadFile(ctx)
-      .then((ctx) => res(ctx))
-      .catch(() => {
-        if (tries < maxRetries) {
-          res(downloadRunner(ctx, tries + 1));
-        } else {
-          rej();
-        }
-      });
-  });
-};
-
+/**
+ * Remove size information from the media node
+ * It is useless in Gatsby context
+ */
 const removeSizes = ({ e, fileNodeID, ...ctx }) =>
   new Promise((res, rej) => {
     if (fileNodeID) {
@@ -81,14 +118,21 @@ const removeSizes = ({ e, fileNodeID, ...ctx }) =>
     res(e);
   });
 
-// Downloads media files and removes "sizes" data as useless in Gatsby context.
+/**
+ * Entry point
+ * Resolve and download all files before continue
+ */
 const downloadMediaFiles = (ctx) =>
   Promise.all(
     ctx.entities.map(
       (e) =>
         new Promise((res, rej) => {
           if (e.__type === `wordpress__wp_media`) {
-            return pipeAsync(reuseFileNode, downloadRunner, removeSizes)({
+            return pipeAsync(
+              reuseFileNode,
+              downloadRunner(downloadFile),
+              removeSizes
+            )({
               e,
               ...ctx,
             }).then((e) => res(e));
