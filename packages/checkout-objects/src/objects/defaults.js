8a48f58aeeaea52a96f0e70e1e344e9e50ba0ac2
njmyers <17010801+njmyers@users.noreply.github.com>
2018-11-14 18:33:03 -0500

    referring to stage now

--- packages/checkout-objects/src/objects/defaults.js
+++ packages/checkout-objects/src/objects/defaults.js
@@ -2,6 +2,8 @@
 type Defaults = <A, B>(prod: A, dev: B) => A | B;
 
 const defaults: Defaults = (prod, dev) =>
-  process.env.NODE_ENV !== 'production' ? dev : prod;
+  process.env.STAGE !== 'production' || process.env.STAGE !== 'staging'
+    ? dev
+    : prod;
 
 export default defaults;
