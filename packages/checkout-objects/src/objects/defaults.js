90a52c38c3132204d805d24f31db91bfed6e69b0
njmyers <17010801+njmyers@users.noreply.github.com>
2018-11-15 17:35:00 -0500

    checking stage and front end gatsby stage

--- packages/checkout-objects/src/objects/defaults.js
+++ packages/checkout-objects/src/objects/defaults.js
@@ -1,9 +1,14 @@
 /** @flow */
 type Defaults = <A, B>(prod: A, dev: B) => A | B;
 
-const defaults: Defaults = (prod, dev) =>
-  process.env.STAGE === 'production' || process.env.STAGE === 'staging'
-    ? prod
-    : dev;
+const defaults: Defaults = (prod, dev) => {
+  const stage = process.env.STAGE
+    ? process.env.STAGE
+    : process.env.GATSBY_STAGE
+    ? process.env.GATSBY_STAGE
+    : 'development';
+
+  stage === 'production' || stage === 'staging' ? prod : dev;
+};
 
 export default defaults;
