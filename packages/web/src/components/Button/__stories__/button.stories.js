e33ece830de12c0ba1d4a9ad78a805d671c3ce15
njmyers <17010801+njmyers@users.noreply.github.com>
2018-12-02 17:54:16 -0500

    added basic button tests and stories

---
+++ packages/web/src/components/Button/__stories__/button.stories.js
@@ -0,0 +1,7 @@
+/** @flow */
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from '../Button';
+
+storiesOf('Button', module).add('Demo', () => <Button />);
