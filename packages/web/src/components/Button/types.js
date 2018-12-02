533f4102a28483d085807d85bd52d518908586f0
njmyers <17010801+njmyers@users.noreply.github.com>
2018-12-02 18:32:24 -0500

    fixed types

--- packages/web/src/components/Button/types.js
+++ packages/web/src/components/Button/types.js
@@ -4,9 +4,9 @@ import * as React from 'react';
 export type Props = {
   children?: React.Node,
   type: 'submit' | 'reset' | 'button',
-  className: string,
-  name: string,
-  onClick: (e: SyntheticEvent<HTMLButtonElement>) => any,
-  margin: boolean,
-  fullWidth: boolean,
+  className?: string,
+  name?: string,
+  onClick?: null | ((e: SyntheticEvent<HTMLButtonElement>) => any),
+  margin?: boolean,
+  fullWidth?: boolean,
 };
