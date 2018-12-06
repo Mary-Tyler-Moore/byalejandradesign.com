1a5bb0fe27bd54c5b5a97bf50e8465b6f795b12a
njmyers <17010801+njmyers@users.noreply.github.com>
2018-12-06 00:39:31 -0500

    updated header image croppings

--- packages/web/src/components/Header/DesktopHeader.js
+++ packages/web/src/components/Header/DesktopHeader.js
@@ -4,17 +4,25 @@ import Img from 'gatsby-image';
 import './header.sass';
 import ByAlejandra from './ByAlejandra.js';
 
-import type { ImageNode } from 'data';
+import type { ImageNode } from '@byalejandradesign/data-objects';
 
 type Props = {
   headerImage: ImageNode,
+  verticalPostion: string,
 };
 
 class DesktopHeader extends React.PureComponent<Props> {
+  static defaultProps = {
+    verticalPosition: '15%',
+  };
+
   render() {
     return (
       <header className="header">
-        <figure className="header_imgContainer">
+        <figure
+          className="header_imgContainer"
+          // style={{ top: this.props.verticalPosition }}
+        >
           {this.props.headerImage.localFile ? (
             <Img
               className="header_img"
@@ -33,7 +41,7 @@ class DesktopHeader extends React.PureComponent<Props> {
 export const query = graphql`
   fragment HeaderImageFragment on File {
     childImageSharp {
-      fluid(maxWidth: 1920, quality: 70, cropFocus: NORTH) {
+      fluid(maxWidth: 1920, quality: 70) {
         ...GatsbyImageSharpFluid
       }
     }
