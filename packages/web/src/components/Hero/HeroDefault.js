8675d6068f675afdb79db7c1dc992d9493ed2fcc
njmyers <17010801+njmyers@users.noreply.github.com>
2019-02-10 18:52:56 -0500

    Cleaned up mobile styles

--- packages/web/src/components/Hero/HeroDefault.js
+++ packages/web/src/components/Hero/HeroDefault.js
@@ -7,7 +7,7 @@ import './hero-default.sass';
 
 const HeroDefault = () => (
   <Hero filter={0.3}>
-    <section className="hero-default">
+    <section className="hero_content-default">
       <h1 className="hero_title-default">{`Colorful\u00A0Ceramics`}</h1>
       <h2 className="hero_subtitle-default">Geometric Patterns & Textures</h2>
     </section>
