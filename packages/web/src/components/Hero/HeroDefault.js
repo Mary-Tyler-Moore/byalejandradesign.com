755c753697c7daae6137768c9d151148f3f15185
njmyers <17010801+njmyers@users.noreply.github.com>
2019-03-31 19:33:32 -0400

    Fixing hero components

--- packages/web/src/components/Hero/HeroDefault.js
+++ packages/web/src/components/Hero/HeroDefault.js
@@ -1,39 +1,4 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Hero from './Hero';
-import Button from '../Button';
-import withSize from 'react-size-components';
 
-import './hero-default.sass';
-
-class HeroDefault extends React.Component {
-  get style() {
-    return !this.props.mobile
-      ? {
-          fontSize: '1.2rem',
-          marginBottom: 2,
-        }
-      : {};
-  }
-
-  render() {
-    return (
-      <Hero filter={0.3}>
-        <section className="hero_content-default">
-          <h1 className="hero_title-default">{`Complex\u00A0Designs`}</h1>
-          <h2 className="hero_subtitle-default">Colorful Textured Ceramics</h2>
-        </section>
-        <section className="hero_cta-default">
-          <Link className="hero_link-default" to={`/shop`}>
-            <Button
-              className="clearButton"
-              span={{ style: this.style }}
-            >{`Shop\u00A0Now`}</Button>
-          </Link>
-        </section>
-      </Hero>
-    );
-  }
-}
-
-export default withSize({ mobile: true })(HeroDefault);
+export default () => <Hero fill="half" filter={0.2} />;
