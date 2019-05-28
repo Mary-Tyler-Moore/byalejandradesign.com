3f770ac3a17771b8258bb2785631d9314d8e8940
njmyers <17010801+njmyers@users.noreply.github.com>
2019-05-27 21:08:20 -0400

    Access circle branch variable directly

--- scripts/modules/set-env.js
+++ scripts/modules/set-env.js
@@ -11,6 +11,22 @@ function setEnv() {
     process.env.STAGE = 'development';
   }
 
+  if (process.env.CIRCLE_BRANCH) {
+    const { CIRCLE_BRANCH } = process.env;
+
+    // assign deployment stage based on circle branches
+    switch (CIRCLE_BRANCH) {
+      case 'production':
+        process.env.STAGE = 'production';
+        break;
+      case 'staging':
+        process.env.STAGE = 'staging';
+        break;
+      default:
+        break;
+    }
+  }
+
   const stage = process.env.STAGE;
 
   process.env.GATSBY_INVALIDATE = Date.now();
